1ec1bd71a6397aec2223771a71740b1f8eb51145
Tomoaki Tsuruta <england0112@gmail.com>
2016-11-25 12:47:39 -0800

    Create user form

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,12 +1,32 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
 
+  alias Rumbl.User, as: User
+
   def index(conn, _params) do
-    users = Repo.all(Rubml.User)
+    users = Repo.all(User)
     render conn, "index.html", users: users
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
   end
 
   def show(conn, _params) do
   end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
 
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -1,4 +1,18 @@
 defmodule Rumbl.User do
   use Rumbl.Web, :model
-  defstruct [:id, :name, :username, :password]
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps
+  end
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, ~w(name username), [])
+    |> validate_length(:username, min: 1, max: 20)
+  end
+
 end
