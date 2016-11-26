8f1724677629550ac244c7902de7f0cf54e5b826
Tomoaki Tsuruta <england0112@gmail.com>
2016-11-25 16:00:51 -0800

    Create session controller

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Rumbl.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Rumbl.Auth, repo: Rumbl.Repo
   end
 
   pipeline :api do
@@ -15,7 +16,8 @@ defmodule Rumbl.Router do
 
   scope "/", Rumbl do
     pipe_through :browser # Use the default browser stack
-
+    get "/", PageController, :index
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
     resources "/users", UserController, only: [:index, :new, :create, :show]
   end
 
